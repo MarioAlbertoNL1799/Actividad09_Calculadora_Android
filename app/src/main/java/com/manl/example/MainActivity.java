83f2035b785a4c2a2d3a3c2520967d113b2443b3
MarioAlbertoNL1799 <1717110635@utectulancingo.edu.mx>
2019-02-28 11:40:54 -0600

    Configuración de diseño

--- app/src/main/java/com/manl/example/MainActivity.java
+++ app/src/main/java/com/manl/example/MainActivity.java
@@ -2,6 +2,7 @@ package com.manl.example;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +11,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void sumar(View view){ }
+    public void restar(View view){ }
+    public void multiplicar(View view){ }
+    public void dividir(View view){ }
+    public void modulo(View view){ }
 }
