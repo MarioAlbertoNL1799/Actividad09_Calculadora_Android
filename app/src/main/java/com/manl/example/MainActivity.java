e92321d40ae5e501f5d554ab97360ce9d64d33e2
MarioAlbertoNL1799 <1717110635@utectulancingo.edu.mx>
2019-02-28 22:26:58 -0600

    Creación de metodos de operaciones elaboradas

--- app/src/main/java/com/manl/example/MainActivity.java
+++ app/src/main/java/com/manl/example/MainActivity.java
@@ -1,24 +1,54 @@
 package com.manl.example;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     private float numero1;
     private float numero2;
     private float resultado;
+    private EditText val_1,val_2,fin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        val_1 = findViewById(R.id.et_num1);
+        val_2 = findViewById(R.id.et_num2);
+        fin = findViewById(R.id.et_resultado);
     }
     public void accionSumar(View view){
-
+        numero1 = Float.parseFloat(val_1.getText().toString());
+        numero2 = Float.parseFloat(val_2.getText().toString());
+        resultado = numero1 + numero2;
+        fin.setText(String.valueOf(resultado));
     }
-    public void accionRestar(View view){ }
-    public void accionMultiplicar(View view){ }
-    public void accionDividir(View view){ }
-    public void accionModulo(View view){ }
+    public void accionRestar(View view){
+        numero1 = Float.parseFloat(val_1.getText().toString());
+        numero2 = Float.parseFloat(val_2.getText().toString());
+        resultado = numero1 - numero2;
+        fin.setText(String.valueOf(resultado));
+    }
+    public void accionMultiplicar(View view){
+        numero1 = Float.parseFloat(val_1.getText().toString());
+        numero2 = Float.parseFloat(val_2.getText().toString());
+        resultado = numero1 * numero2;
+        fin.setText(String.valueOf(resultado));
+    }
+    public void accionDividir(View view){
+        numero1 = Float.parseFloat(val_1.getText().toString());
+        numero2 = Float.parseFloat(val_2.getText().toString());
+        resultado = numero1 / numero2;
+        fin.setText(String.valueOf(resultado));
+    }
+    public void accionModulo(View view){
+        numero1 = Float.parseFloat(val_1.getText().toString());
+        numero2 = Float.parseFloat(val_2.getText().toString());
+        resultado = numero1 % numero2;
+        fin.setText(String.valueOf(resultado));
+    }
 }
