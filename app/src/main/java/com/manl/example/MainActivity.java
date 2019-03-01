ee84f2192c155b9418273c6c2bf79e244bf1e170
MarioAlbertoNL1799 <1717110635@utectulancingo.edu.mx>
2019-02-28 21:24:51 -0600

    Diseño finalizado, icono arreglado

--- app/src/main/java/com/manl/example/MainActivity.java
+++ app/src/main/java/com/manl/example/MainActivity.java
@@ -5,15 +5,20 @@ import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private float numero1;
+    private float numero2;
+    private float resultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void sumar(View view){ }
-    public void restar(View view){ }
-    public void multiplicar(View view){ }
-    public void dividir(View view){ }
-    public void modulo(View view){ }
+    public void accionSumar(View view){
+
+    }
+    public void accionRestar(View view){ }
+    public void accionMultiplicar(View view){ }
+    public void accionDividir(View view){ }
+    public void accionModulo(View view){ }
 }
